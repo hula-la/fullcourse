df2be5261caae826f34e386a36e31f257898716f
hoju <ghwnsdl999@naver.com>
2022-09-06 12:17:44 +0900

    ✨ 구조 설계
    # <이모지> <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    ✨ 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - 구조 설계
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # Jira Smart commit 사용 시 Jira 이슈 또한 종료할 수 있음.
    # 예) Close #7
    
    ################
    # ✨ : 새로운 기능 추가
    # 🐛 : 버그 수정
    # 📚 : 문서 수정
    # 🚨 : 테스트 코드 추가
    # 🔨 : 코드 리팩토
    # 📝 : 코드 의미에 영향을 주지 않는 변경사항
    # 🔧 : 기타 변경사항, 프로`덕션 코드 변경사항 없음
    ################

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+// Main
+import Layout from "./layout/Layout";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* Main */}
+        <Route path="" element={<MainPage />} />
+        {/* user */}
+        <Route path="user" element={<Layout />}></Route>
+        {/* trip */}
+        <Route path="trip" element={<Layout />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <p>레이아웃</p>
+    </div>
+  );
+};
+
+export default Layout;
