3542ff811842211f2775082c34aa401e09531181
손효재 <sonjw5128@gmail.com>
2022-09-06 13:50:29 +0900

    [Add] backend project

---
+++ backend/src/test/java/com/ssafy/fullcourse/FullcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.fullcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
