a9e05bc9602e2bda1c0d5d78a2785b29e399abd9
손효재 <sonjw5128@gmail.com>
2022-09-13 17:28:35 +0900

    [BE] Update : security config
    # <이모지> <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # [Add] 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    # - 회원 정보 확인
    # - 토큰 발급 및 저장
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # Jira Smart commit 사용 시 Jira 이슈 또한 종료할 수 있음.
    # 예) Close #7
    
    ################
    ### Code 관련
    # Add : 코드나 문자 추가
    # Test : 테스트 코드 삽입
    # Update : Fix와 달리 원래 정상적으로 동작한 기능의 보완 개념
    # Fix : 버그 수정
    # Remove : 코드 삭제
    # Refactor : 결과의 변경 없이 코드 구조 재조정
    #                Ex) createStore의 함수를 작은 함수로 분리
    # Move : 코드나 파일의 이동
    # Style : 서식, 세미콜론 누락
    # Correct : 문법 오류, 타입 및 변수 이름 변경 등 수정 사항에 사용
    
    ### 기능 관련
    # Feat : 새로운 기능 추가
    # Improve : 호환성, 테스트 커버리지(테스트 케이스가 얼마나 충족 한지), 접근성 향상
    # Chore : 기타 작업, 빌드 작업, 패키지 관리자 구성 등 업데이트, Production Code 변경 없음
    # Test : git 관련 테스트 혹은 코드 관련 테스트 진행
    
    ### 파일, 폴더 관련
    # Docs : 문서 변경 (문서 추가, 이름 변경 등)
    # Move : 코드나 파일의 이동
    # Rename : 이름 변경
    # Create : 파일이나 폴더 추가
    ################

--- backend/src/main/java/com/ssafy/fullcourse/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/fullcourse/global/config/SecurityConfig.java
@@ -1,35 +1,72 @@
 package com.ssafy.fullcourse.global.config;
 
+import com.ssafy.fullcourse.global.util.JwtAccessDeniedHandler;
+import com.ssafy.fullcourse.global.util.JwtAuthenticationEntryPoint;
+import com.ssafy.fullcourse.global.util.JwtSecurityConfig;
+import com.ssafy.fullcourse.global.util.TokenProvider;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-/**
- * 인증(authentication) 와 인가(authorization) 처리를 위한 스프링 시큐리티 설정 정의.
- */
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메서드 단위로 추가하기위해 적용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    // Password 인코딩 방식에 BCrypt 암호화 방식 사용
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().permitAll()
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                // exception을 핸들링할때 만들었던 에러처리 클래스들을 추가해준다.
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
                 .and()
-                .csrf().disable();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                /**
+                 * security
+                 */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().permitAll()
+                // 인증없이 접근을 허용하는 path 추가
+//                .and()
+//                .authorizeRequests()
+//                // 나머지 요청은 모두 인증을 받는다
+//                .anyRequest().authenticated()
+
+
+                // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig 클래스도 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
     }
 }
