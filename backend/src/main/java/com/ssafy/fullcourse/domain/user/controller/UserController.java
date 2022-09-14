2e7aaa01c5c2d646d04c3a9ebf8ede26aba316c9
손효재 <sonjw5128@gmail.com>
2022-09-14 12:14:17 +0900

    [BE] Add : user manage & aws s3 config

--- backend/src/main/java/com/ssafy/fullcourse/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/fullcourse/domain/user/controller/UserController.java
@@ -2,12 +2,18 @@ package com.ssafy.fullcourse.domain.user.controller;
 
 import com.ssafy.fullcourse.domain.user.application.KakaoUserService;
 import com.ssafy.fullcourse.domain.user.application.NaverUserService;
+//import com.ssafy.fullcourse.domain.user.application.UserManageService;
+import com.ssafy.fullcourse.domain.user.application.UserManageService;
 import com.ssafy.fullcourse.domain.user.dto.UserDto;
+import com.ssafy.fullcourse.global.model.BaseResponseBody;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
@@ -19,6 +25,7 @@ public class UserController {
 
     private final KakaoUserService kakaoUserService;
     private final NaverUserService naverUserService;
+    private final UserManageService userManageService;
 
     @PostMapping("/kakao")
     public HttpEntity<?> kakaoLogin(@RequestBody HashMap<String, String> param) {
@@ -32,6 +39,24 @@ public class UserController {
         naverUserService.getUserInfoByAccessToken(param.get("access_token"));
         UserDto userDto = naverUserService.getUserInfoByAccessToken(param.get("access_token"));
         return naverUserService.login(userDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponseBody> getInfo(HttpServletRequest request) {
+        return userManageService.getInfo(request);
+    }
+
+    @PostMapping
+    public ResponseEntity<BaseResponseBody> modify(
+            @RequestPart(value = "file", required = false) MultipartFile file,
+            @RequestPart(value = "userDto", required = false) UserDto userDto,
+            HttpServletRequest request) {
+        return userManageService.modify(userDto,file,request);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<BaseResponseBody> delete(HttpServletRequest request) {
+        return userManageService.delete(request);
     }
 
 }
