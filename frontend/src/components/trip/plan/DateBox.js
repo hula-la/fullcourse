98f1c4845ef009ffbe900a2c1b94f87fd3eefbbe
김누리 <zsda3131@naver.com>
2022-09-21 00:50:07 +0900

    [FE] Feat: 여행 일정 달력 기능

--- frontend/src/components/trip/plan/DateBox.js
+++ frontend/src/components/trip/plan/DateBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DateRanger from './DateRanger';
 
 const DateBox = () => {
   return (
@@ -6,6 +7,8 @@ const DateBox = () => {
       {/* 계산한 데이 값 */}
       {/* 달력 이모티콘 */}
       {/* 시작날짜 끝나는 날짜 */}
+      <DateRanger></DateRanger>
+
     </div>
   );
 };
