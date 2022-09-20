23372993eb871494a75ca0653db7958d3cc0ba0f
김누리 <zsda3131@naver.com>
2022-09-20 14:34:40 +0900

    [FE] Create: 일정 생성 페이지 추가

---
+++ frontend/src/components/trip/plan/DateBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DateBox = () => {
+  return (
+    <div>
+      {/* 계산한 데이 값 */}
+      {/* 달력 이모티콘 */}
+      {/* 시작날짜 끝나는 날짜 */}
+    </div>
+  );
+};
+
+export default DateBox;
