24285e0b0245268c84b0ef4b9c5cffdde538115e
손효재 <sonjw5128@gmail.com>
2022-09-22 16:27:58 +0900

    [FE] Update : front base url
    front baseurl 수정

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://j7e106.p.ssafy.io:8081/api',
+  baseURL: 'https://j7e106.p.ssafy.io:8081',
   // baseURL: 'http://127.0.0.1:8080',
   headers: {
     'Content-Type': 'application/json',
