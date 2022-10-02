e1d6379cd862f4944a77dcf33c5864e42c9e8bf5
soojin <ksgf988@naver.com>
2022-10-03 02:48:51 +0900

    [FE] Update: 공유 디테일 페이지 반응형

---
+++ frontend/src/components/share/mobile/MobileDetailHeader.js
@@ -0,0 +1,205 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  makeDayTagList,
+} from '../../../features/share/shareSlice';
+import { createSharedFcLike } from '../../../features/share/shareActions';
+// import MobileComment from './MobileComment';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+import CommentIcon from '@mui/icons-material/Comment';
+import MobileComment from './MobileComment';
+
+import ClearIcon from '@mui/icons-material/Clear';
+
+const Wrapper = styled.div`
+  justify-content: center;
+
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  height: 15vh;
+
+  position: relative;
+
+  .mobileComment{
+    position: absolute;
+    left: 0;
+    top: 0; 
+    width: 100%;
+    height: 100%;
+  }
+
+  .hidden{
+    visibility: hidden;
+  }
+
+  .clearIconBox{
+    right: 1rem;
+    top:0.2rem;
+    position: absolute;
+    z-index: 10;
+  }
+
+  #userInfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    font-size: small;
+    /* font-weight: ; */
+  }
+
+  #imgBlock {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #profileImg {
+    width: 3.5rem;
+    height: 3.5rem;
+    border-radius: 20px;
+  }
+
+  .commentIcon{
+    margin-left:1rem;
+    /* display: inline-block; */
+  }
+
+`;
+
+const ShareInfo = styled.div`
+  margin-left: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .favorite {
+    cursor: pointer;
+    color: #f73131;
+    margin-left: 10px;
+  }
+
+  .favoriteborder {
+    cursor: pointer;
+    color: #f73131;
+    margin-left: 10px;
+  }
+
+  .countArea {
+    margin-top: 10px;
+  }
+
+  
+`;
+
+
+const SharedTitle = styled.div`
+  display: flex;
+  text-align: left;
+  padding: 1.5rem 1rem;
+  align-items: center;
+  justify-content: space-around;
+
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.3rem;
+    font-weight: bold;
+  }
+
+  #userNickName {
+    margin-top: 0.5rem;
+    font-size: 1rem;
+    color: #333333a3;
+    font-weight: bold;
+  }
+`;
+
+const MobileDetailHeader = ({ sharedFcInfo }) => {
+  const [isComment, setIsComment] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (sharedFcInfo) {
+      dispatch(makeDayTagList(sharedFcInfo.day));
+    }
+  }, [sharedFcInfo]);
+
+  const onClickLike = () => {
+    dispatch(createSharedFcLike(sharedFcInfo.sharedFcId));
+  };
+
+  const toggleActive = (e) => {
+    setIsComment((prev) => {
+      return !prev;
+    });
+  };
+
+  return (
+    <Wrapper>
+      <div className={`clearIconBox ${isComment?"":"hidden"}`}
+      onClick={toggleActive}>
+
+      <ClearIcon />
+      </div>
+      {sharedFcInfo ? (
+        <SharedTitle>
+          <div id="userInfo">
+            <div id="imgBlock">
+              <img
+                id="profileImg"
+                src={sharedFcInfo.user.imgUrl}
+                alt="profileImg"
+              />
+            </div>
+            <div id="userNickName">{sharedFcInfo.user.nickname}</div>
+          </div>
+          <ShareInfo>
+            <div>
+              <div className="title">
+                {sharedFcInfo.title}
+                {sharedFcInfo ? (
+                  <>
+                    {sharedFcInfo.like ? (
+                      <FavoriteIcon
+                        className="favorite"
+                        onClick={onClickLike}
+                      />
+                    ) : (
+                      <FavoriteBorderIcon
+                        className="favoriteborder"
+                        onClick={onClickLike}
+                      />
+                    )}
+                  </>
+                ) : null}
+              </div>
+              <div className="countArea">
+                조회수 {sharedFcInfo.viewCnt} , 좋아요 {sharedFcInfo.likeCnt} 개
+              </div>
+            </div>
+          </ShareInfo>
+          <div className={`commentIcon ${isComment?"hidden":""}`} onClick={toggleActive}>
+            <CommentIcon/>
+          </div>
+          <div className={`mobileComment ${isComment ? "" : "hidden"}`}>
+          <MobileComment
+            sharedFcInfo={sharedFcInfo}
+          />
+
+          </div>
+
+        </SharedTitle>
+      ) : null}
+    </Wrapper>
+  );
+};
+
+export default MobileDetailHeader;
---
+++ frontend/src/components/share/mobile/MobileFullcourseMap.js
@@ -0,0 +1,138 @@
+/* global kakao */
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux/es/exports';
+import styled from 'styled-components';
+
+const MapBlock = styled.div`
+  width: 100%;
+  height: 30vh;
+  #map {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const FullcourseMap = () => {
+  const { fullcourseDetail } = useSelector((state) => state.trip);
+  const { checkedDay } = useSelector((state) => state.share);
+  const [markerList, setMarkerList] = useState([]);
+  const [linePath, setLinePath] = useState([]);
+
+  useEffect(() => {
+    if (fullcourseDetail) {
+      let newMarkerList = [];
+      Object.keys(fullcourseDetail.places).map((place, index) => {
+        const tmp = fullcourseDetail.places[place].map((pla, index) => {
+          return {
+            title: pla.place.name,
+            latlng: new kakao.maps.LatLng(pla.place.lat, pla.place.lng),
+          };
+        });
+        newMarkerList = [...newMarkerList, tmp];
+      });
+      setMarkerList(newMarkerList);
+    } else {
+    }
+  }, [fullcourseDetail]);
+
+  useEffect(() => {
+    if (fullcourseDetail) {
+      let newLinePath = [];
+      Object.keys(fullcourseDetail.places).map((place, index) => {
+        const tmp = fullcourseDetail.places[place].map((pla, index) => {
+          return new kakao.maps.LatLng(pla.place.lat, pla.place.lng);
+        });
+        newLinePath = [...newLinePath, tmp];
+      });
+      setLinePath(newLinePath);
+    } else {
+      console.log(3);
+    }
+  }, [fullcourseDetail]);
+
+  useEffect(() => {
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(35.17962489619582, 129.07480154639234),
+      level: 10,
+    };
+
+    var map = new kakao.maps.Map(container, options);
+
+    if (checkedDay === 6) {
+      for (var i = 0; i < markerList.length; i++) {
+        for (var j = 0; j < markerList[i].length; j++) {
+          // 지도에 표시할 선을 생성합니다
+          var polyline = new kakao.maps.Polyline({
+            path: linePath, // 선을 구성하는 좌표배열 입니다
+            strokeWeight: 5, // 선의 두께 입니다
+            strokeColor: '#041688', // 선의 색깔입니다
+            strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'solid', // 선의 스타일입니다
+          });
+
+          // 지도에 선을 표시합니다
+          polyline.setMap(map);
+          // 마커 이미지의 이미지 크기 입니다
+          var imageSize = new kakao.maps.Size(30, 40);
+
+          // 마커 이미지를 생성합니다
+          var markerImage = new kakao.maps.MarkerImage(
+            `/img/marker/marker${i}.png`,
+            imageSize,
+          );
+
+          // 마커를 생성합니다
+          var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: markerList[i][j].latlng, // 마커를 표시할 위치
+            title: markerList[i][j].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage, // 마커 이미지
+          });
+        }
+      }
+    } else {
+      for (var i = 0; i < markerList.length; i++) {
+        if (checkedDay === i) {
+          for (var j = 0; j < markerList[i].length; j++) {
+            // 지도에 표시할 선을 생성합니다
+            var polyline = new kakao.maps.Polyline({
+              path: linePath[i], // 선을 구성하는 좌표배열 입니다
+              strokeWeight: 5, // 선의 두께 입니다
+              strokeColor: '#041688', // 선의 색깔입니다
+              strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+              strokeStyle: 'solid', // 선의 스타일입니다
+            });
+
+            // 지도에 선을 표시합니다
+            polyline.setMap(map);
+            // 마커 이미지의 이미지 크기 입니다
+            var imageSize = new kakao.maps.Size(30, 40);
+
+            // 마커 이미지를 생성합니다
+            var markerImage = new kakao.maps.MarkerImage(
+              `/img/marker/marker${i}.png`,
+              imageSize,
+            );
+
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+              map: map, // 마커를 표시할 지도
+              position: markerList[i][j].latlng, // 마커를 표시할 위치
+              title: markerList[i][j].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+              image: markerImage, // 마커 이미지
+            });
+          }
+        }
+      }
+    }
+  }, [markerList, checkedDay]);
+
+  return (
+    <MapBlock>
+      <div id="map"></div>
+    </MapBlock>
+  );
+};
+
+export default FullcourseMap;
