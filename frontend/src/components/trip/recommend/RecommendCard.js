0f8d3d300289191adacad788ff2ee99e93efae7c
soojin <ksgf988@naver.com>
2022-10-04 21:21:49 +0900

    [FE] Add: 추천 페이지에 워드클라우드 추가
    추천페이지 hover하면 워드클라우드 생기도록

--- frontend/src/components/trip/recommend/RecommendCard.js
+++ frontend/src/components/trip/recommend/RecommendCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect}  from 'react';
 import styled from 'styled-components';
 import { likePlace, deletePlace } from '../../../features/survey/surveySlice';
 import { useSelector, useDispatch } from 'react-redux';
@@ -7,9 +7,73 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { FaCommentDots } from 'react-icons/fa';
 import { GoHeart } from 'react-icons/go';
 
+import ReactWordcloud from "react-wordcloud";
+import { createWordCloud } from '../../../features/wordcloud/wcAction';
+
+
+const options = {
+  colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
+  enableTooltip: true,
+  deterministic: false,
+  fontFamily: "impact",
+  fontSizes: [5, 60],
+  fontStyle: "normal",
+  fontWeight: "normal",
+  padding: 1,
+  rotations: 3,
+  rotationAngles: [0, 90],
+  scale: "sqrt",
+  spiral: "archimedean",
+  transitionDuration: 1000
+};
+
 const Wrapper = styled.div`
-  background: white;
+  
   position: relative;
+
+  
+  
+  
+  /* 카드 뒤집기 효과 추가 */
+  /* overflow: hidden; */
+  .card {
+    
+  width: 100%; 
+  height: 100%; 
+  position: relative;
+  transition: .4s;
+  transform-style: preserve-3d;
+
+}	
+
+.front, .back {
+  overflow: hidden;
+  border-radius: 1rem;
+    box-shadow: 1px 1px 5px darkgrey;
+    background: white;
+
+  position: absolute;
+  width: 100%; 
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+/* .front {
+  background: red; 
+} */
+
+// 뒷면은 사전에 미리 뒤집기
+.back { 
+  /* background: royalblue;  */
+  transform: rotateY(180deg);
+}
+
+// 호버 시 뒤집기
+:hover .card {
+  transform: rotateY(180deg);
+}
+/* --------------------- */
+
 
   animation: flip-vertical-right 0.7s cubic-bezier(0.455, 0.03, 0.515, 0.955)
     both;
@@ -61,6 +125,7 @@ const Wrapper = styled.div`
     }
   }
 
+
   /* .placeTag::-webkit-scrollbar-track{
 border-radius: 10px;    
 
@@ -71,10 +136,6 @@ border-radius: 10px;
     overflow: scroll;
   }
 
-  border-radius: 1rem;
-  overflow: hidden;
-
-  box-shadow: 1px 1px 5px darkgrey;
 
   img {
     width: 100%;
@@ -144,6 +205,27 @@ const RecommendCard = ({ place, index }) => {
   const dispatch = useDispatch();
   const { recommendPlaceList } = useSelector((state) => state.survey);
   const { likePlaceIndex } = useSelector((state) => state.survey);
+  const [words, setWords] = useState('')
+
+  useEffect(() => {
+    console.log(place.name);
+    dispatch(createWordCloud(place.name))
+      .then((result) => {
+        // setWords(result.payload.entries)
+        let data = Object.entries(result.payload).map((entry) => {
+          return {
+            text: entry[0],
+            value: entry[1]
+          };
+        });
+
+        setWords(data);
+      })
+  },[])
+
+  // useEffect(() => {
+  //   console.log(words)
+  // }, [words])
 
   const onClickAdd = (index, e) => {
     //  이미 선택한 장소면 삭제
@@ -162,29 +244,41 @@ const RecommendCard = ({ place, index }) => {
 
   return (
     <Wrapper>
-      <img src={place.imgUrl} />
-      <div className="cardHead">
-        <div></div>
-        <div className="placeName">{place.name}</div>
-        <div>
-          <div className="button" onClick={(e) => onClickAdd(index, e)}>
-            {likePlaceIndex.includes(place.placeId) ? (
-              <AddCircleIcon />
-            ) : (
-              <AddCircleOutlineIcon />
-            )}
+      <div className='card'>
+        {/* 앞면 */}
+        <div className='front'>
+          <img src={place.imgUrl} />
+          <div className="cardHead">
+            <div></div>
+            <div className="placeName">{place.name}</div>
+            <div>
+              <div className="button" onClick={(e) => onClickAdd(index, e)}>
+                {likePlaceIndex.includes(place.placeId) ? (
+                  <AddCircleIcon />
+                ) : (
+                  <AddCircleOutlineIcon />
+                )}
+              </div>
+            </div>
           </div>
+          <div className="placeTag">
+            {place.tag.split(',').map((tag) => {
+              return <Tag>#{tag}</Tag>;
+            })}
+          </div>
+          <CardFooter>
+            <Like /> <p>{place.likeCnt}</p>
+            <Comment /> <p>{place.reviewCnt}</p>
+          </CardFooter>
+
         </div>
+{/* 뒷면 */}
+        <div className='back'>
+        <ReactWordcloud options={options} words={words} />
+
+        </div>
+
       </div>
-      <div className="placeTag">
-        {place.tag.split(',').map((tag) => {
-          return <Tag>#{tag}</Tag>;
-        })}
-      </div>
-      <CardFooter>
-        <Like /> <p>{place.likeCnt}</p>
-        <Comment /> <p>{place.reviewCnt}</p>
-      </CardFooter>
     </Wrapper>
   );
 };
